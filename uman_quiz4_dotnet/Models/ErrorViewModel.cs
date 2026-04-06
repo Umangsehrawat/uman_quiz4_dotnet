eddfd0d8023d89fa5e3836081f03f652ae02d953
Umang Sehrawat <usehrawa@my.centennialcollege.ca>
2026-04-06 10:16:02 -0400

    Initial commit

---
+++ uman_quiz4_dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace uman_quiz4_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
